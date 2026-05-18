384167888cdca421dba0cd929f35e87816d072a0
Air Launcher Developer <dev@airlauncher.local>
2026-05-18 23:35:06 +0300

    Release v1.1.12 cloud launcher icon

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,11 @@
+import appIcon from '../../assets/air-launcher-cloud-icon.png'
+
 function Header() {
   return (
     <header className="header">
       <div className="header-content">
         <div className="brand-mark" aria-hidden="true">
-          <span />
+          <img src={appIcon} alt="" />
         </div>
         <div>
           <h1 className="header-title">Air Launcher</h1>
