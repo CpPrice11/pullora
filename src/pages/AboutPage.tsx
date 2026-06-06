d7e259b02c7a2f866d96451b9a3498d4149594c2
Air Launcher Developer <dev@airlauncher.local>
2026-06-06 19:19:34 +0300

    Release v4.1.2 About SAM layout fix

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -17,7 +17,7 @@ import './PageStyles.css'
 
 const LAUNCHER_OWNER = 'CpPrice11'
 const LAUNCHER_REPO = 'air-launcher'
-const FALLBACK_CURRENT_VERSION = 'v4.1.1'
+const FALLBACK_CURRENT_VERSION = 'v4.1.2'
 
 type PendingLauncherAction = {
   release: GitHubRelease
