9879561577bfa6789f35b238e910f6d5a8099fac
Air Launcher Developer <dev@airlauncher.local>
2026-06-25 15:28:51 +0300

    Style library inline panels in cinematic shell

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -18,7 +18,7 @@ import './PageStyles.css'
 
 const LAUNCHER_OWNER = 'CpPrice11'
 const LAUNCHER_REPO = 'pullora'
-const FALLBACK_CURRENT_VERSION = 'v5.2.20'
+const FALLBACK_CURRENT_VERSION = 'v5.2.21'
 
 type PendingLauncherAction = {
   release: GitHubRelease
