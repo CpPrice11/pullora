f65f460b8f271d57d22ba5e430850846dd826028
Air Launcher Developer <dev@airlauncher.local>
2026-06-29 14:27:15 +0300

    Refine Fluent library layout

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -18,7 +18,7 @@ import './PageStyles.css'
 
 const LAUNCHER_OWNER = 'CpPrice11'
 const LAUNCHER_REPO = 'pullora'
-const FALLBACK_CURRENT_VERSION = 'v5.2.33'
+const FALLBACK_CURRENT_VERSION = 'v5.2.34'
 
 type PendingLauncherAction = {
   release: GitHubRelease
