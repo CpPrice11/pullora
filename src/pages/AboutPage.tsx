09dd5ec98026f304e9221063c4cb19ca78ffcbc2
Air Launcher Developer <dev@airlauncher.local>
2026-06-29 13:31:54 +0300

    Align library shell dimensions

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -18,7 +18,7 @@ import './PageStyles.css'
 
 const LAUNCHER_OWNER = 'CpPrice11'
 const LAUNCHER_REPO = 'pullora'
-const FALLBACK_CURRENT_VERSION = 'v5.2.31'
+const FALLBACK_CURRENT_VERSION = 'v5.2.32'
 
 type PendingLauncherAction = {
   release: GitHubRelease
