1c39c5609ab699b2d5d7d77b6498514d2f81d684
Air Launcher Developer <dev@airlauncher.local>
2026-07-01 18:25:21 +0300

    Add acrylic library material

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -18,7 +18,7 @@ import './PageStyles.css'
 
 const LAUNCHER_OWNER = 'CpPrice11'
 const LAUNCHER_REPO = 'pullora'
-const FALLBACK_CURRENT_VERSION = 'v5.2.35'
+const FALLBACK_CURRENT_VERSION = 'v5.2.36'
 
 type PendingLauncherAction = {
   release: GitHubRelease
