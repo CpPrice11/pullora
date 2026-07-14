9273ae064b4923a9d6c0d452040af343625e4d79
Air Launcher Developer <dev@airlauncher.local>
2026-07-14 19:41:46 +0300

    Restore classic Pullora design

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -19,7 +19,7 @@ import './PageStyles.css'
 
 const LAUNCHER_OWNER = 'CpPrice11'
 const LAUNCHER_REPO = 'pullora'
-const FALLBACK_CURRENT_VERSION = 'v5.2.42'
+const FALLBACK_CURRENT_VERSION = 'v5.2.43'
 
 type PendingLauncherAction = {
   release: GitHubRelease
